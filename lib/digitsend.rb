99777b16cfe238ed9c1334c6a328fe5b409ca7a9
Chad Hendry <chendry@chendry.org>
2012-08-31 14:32:56 -0500

    Repository#upload

--- lib/digitsend.rb
+++ lib/digitsend.rb
@@ -21,16 +21,59 @@ module DigitSend
   end
 
   class Client
-    def self.call(path, params)
-      http = Net::HTTP.new(Config.host, Config.port)
-      http.use_ssl = Config.use_ssl
+    class <<self
+      def call(path, params)
+        http = Net::HTTP.new(Config.host, Config.port)
+        http.use_ssl = Config.use_ssl
 
-      response = http.post path, params && params.to_json,
-        'Content-Type' => 'application/json',
-        'Accept' => 'application/vnd.digitsend.v1',
-        'Authorization' => %Q[Token token="#{Config.api_token}"]
+        response = http.post path, params && params.to_json,
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/vnd.digitsend.v1',
+          'Authorization' => %Q[Token token="#{Config.api_token}"]
 
-      response.body.empty? ? nil : JSON.parse(response.body)
+        response.body.empty? ? nil : JSON.parse(response.body)
+      end
+
+      def upload_s3_file(filename, data)
+         response = create_s3_file(filename)
+
+         upload_to_s3 \
+           URI.parse(response["url"]),
+           response["fields"],
+           stream_for_data(filename, data)
+
+         response["uuid"]
+      end
+
+      private
+
+        def create_s3_file(name)
+          Client.call '/api/s3_files', s3_file: { name: name }
+        end
+
+        def upload_to_s3(url, fields, stream)
+          req = Net::HTTP::Post::Multipart.new \
+            url.to_s,
+            fields.merge("file" => UploadIO.new(stream, "binary/octet-stream"))
+
+          n = Net::HTTP.new(url.host, url.port)
+          n.use_ssl = true
+          n.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+          n.start do |http|
+            http.request(req)
+          end
+        end
+
+        def stream_for_data(filename, data)
+          if data.nil?
+            File.open(filename, "r")
+          elsif data.is_a?(String)
+            StringIO.new(data)
+          else
+            data
+          end
+        end
     end
   end
 
@@ -63,42 +106,19 @@ module DigitSend
 
       def s3_file_uuids
         @attachments.collect do |filename, data|
-          s3_file_uuid(filename, stream_for_data(filename, data))
+          Client.upload_s3_file(filename, data)
         end
       end
+  end
 
-      def stream_for_data(filename, data)
-        if data.nil?
-          File.open(filename, "r")
-        elsif data.is_a?(String)
-          StringIO.new(data)
-        else
-          data
-        end
-      end
+  class Repository
+    def self.upload(repo_name, path, filename, data)
+      uuid = Client.upload_s3_file(filename, data)
 
-      def s3_file_uuid(filename, stream)
-         response = create_s3_file(filename)
-         upload_to_s3(URI.parse(response["url"]), response["fields"], stream)
-         response["uuid"]
-      end
-
-      def create_s3_file(name)
-        Client.call '/api/s3_files', s3_file: { name: name }
-      end
-
-      def upload_to_s3(url, fields, stream)
-        req = Net::HTTP::Post::Multipart.new \
-          url.to_s,
-          fields.merge("file" => UploadIO.new(stream, "binary/octet-stream"))
-
-        n = Net::HTTP.new(url.host, url.port)
-        n.use_ssl = true
-        n.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        n.start do |http|
-          http.request(req)
-        end
-      end
+      Client.call '/api/files/versions',
+        repo_name: repo_name,
+        path: path,
+        repo_file_version: { s3_file_uuid: uuid }
+    end
   end
 end
